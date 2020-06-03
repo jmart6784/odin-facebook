f3962950c41f3dcf647b8aab771bf629cf799d67
Juan <jmartinez6784@yahoo.com>
2020-06-02 19:36:18 -0700

    Added Post and User Associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       flash.notice = "Successfully created a Post!"
@@ -30,6 +31,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.user_id = current_user.id
     @post.update(post_params)
     flash.notice = "Post '#{@post.title}' was updated!"
     redirect_to post_path(@post)
