7114722e06a25dab1e3c90de845b496244c8b950
Juan <jmartinez6784@yahoo.com>
2020-06-03 17:34:16 -0700

    Added edit feature for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,14 +10,18 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    # @post = Post.find(params[:id])
-    # @comment = Comment.find(params[:comment_id])
-    # @comment = @post.comment_id
-    # @comment.post_id = @post
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
   end
 
   def update
-    
+    @post = Post.find(params[:post_id])
+
+    @comment = Comment.find(params[:id])
+    @comment.user_id = current_user.id
+    @comment.update(comment_params)
+    flash.notice = "Comment was edited!"
+    redirect_to post_path(@post)
   end
 
   def destroy
@@ -27,6 +31,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :user_id, :post_id)
   end
 end
