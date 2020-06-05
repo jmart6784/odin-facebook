5a03e93a5985e927f972921b18d9b5e47255a233
Juan <jmartinez6784@yahoo.com>
2020-06-05 12:47:31 -0700

    Added letter opener gem, And User_mailer.rb

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url = "http://localhost:3000/"
+    mail(to: @user.email, subject: "Welcome")
+  end
+end
