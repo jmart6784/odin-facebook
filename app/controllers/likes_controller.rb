5c9d1744e630e79624073026ea2b70e0060d12f1
Juan <jmartinez6784@yahoo.com>
2020-06-26 18:24:24 -0700

    Removed useless 'show' likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,4 @@
 class LikesController < ApplicationController
-  def update
-    # Post show likes update
-    @post = Post.find(params[:id])
-    like = Like.where(post_id: @post.id, user_id: current_user.id)
-
-    if like == []
-      @post.likes.create!(user_id: current_user.id, post_id: @post.id)
-      @like_exists = true
-    else
-      like.destroy_all
-      @like_exists = false
-    end
-
-    respond_to do |format|
-      format.html {}
-      format.js {}
-    end
-  end
-
   def index_likes_update
     @post = Post.find(params[:id])
     like = Like.where(post_id: @post.id, user_id: current_user.id)
