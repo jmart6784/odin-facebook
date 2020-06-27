9469616ca8adbc6484a1c07429a10377159b69ec
Juan <jmartinez6784@yahoo.com>
2020-06-26 17:18:03 -0700

    Styled post show, comments form, and edit comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true
-  validates :body, length: { maximum: 6000 }
+  validates :body, length: { maximum: 5000 }
 end
